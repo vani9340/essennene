80282be6a19eff44a260b845e4953381d5aafb74
vani9340 <sg5428702@gmail.com>
2023-07-03 22:12:32 +0530

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'org': '#E7892C',
+        'light-grey':'#9A9AB0'
+      },
+    }
+  },
+  plugins: [],
+}
