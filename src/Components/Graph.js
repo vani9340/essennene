d063ad02659a0a0e9d66f84f840fba6770f45cbf
vani9340 <sg5428702@gmail.com>
2023-07-04 18:46:06 +0530

    changes

--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -2,18 +2,77 @@ import React from 'react'
 import './Graph.css'
 import Divider from '../images/Separator.png'
 import ThreeDot from '../images/Button.png'
- 
+import Chart from '../images/ChartLine.svg'
+import graph from '../images/'
 const Graph = () => {
   return (
-    <div className='box'>
-        <div>
-        <div className='header'>
-        <p style={{fontSize:'16px',fontWeight:'700',marginTop:'5%',marginLeft:'2%'}}>Earning</p>
-        <button style={{marginLeft:'195px'}} className='btn btn-outline-warning dropdown-toggle butn'>Weekly</button><span className='imgdot'><img  src={ThreeDot} alt='button'/></span>
+    <>
+    <div className='Main-box rounded-lg'>
+    <div className='header'>
+        <p style={{fontSize:'16px',fontWeight:'700',width:'132px',marginTop:'5%',marginLeft:'4%'}}>Item Sold</p>
+        <button className='btn btn-outline-warning dropdown-toggle butn' style={{marginLeft:'60%',marginTop:'5%'}}>Best Seller</button>
+        <span style={{marginTop:'5%'}}>
+        <img src={ThreeDot} alt='icon'/>
+    </span>
     </div>
     <img src={Divider} alt='divideSection'/>
+    <div className='parentdiv'>
+        <div className='div1'>
+        <p style={{fontSize:'24px',fontWeight:'700',marginRight:'238px'}}>$50.000</p>
+        <p style={{marginRight:'400px',width:'117px',height:'18px',fontSize:'14px',marginTop:'-18px'}}>Earning Overtime</p>
+        </div>
+        <div className='div2'>
+            <p style={{fontSize:'24px',fontWeight:'700',marginLeft:'218px'}}>1050</p>
+            <p style={{width:'117px',height:'18px',fontSize:'14px',marginTop:'-18px',marginLeft:'224px'}}>Total Order</p>
         </div>
     </div>
+    <div className='parentChart'>
+    <div className='listItems'>
+        <p style={{fontSize:'14px',fontWeight:'700',marginRight:'7%',marginBottom:'3%',marginLeft:'1.5%'}}>$50.000</p>
+        <span><img src={Chart} alt='line' /></span>
+    </div>
+    <div className='listItems'>
+        <p style={{fontSize:'14px',fontWeight:'700',marginRight:'7%',marginBottom:'3%',marginLeft:'1.5%'}}>$45.000</p>
+        <span><img src={Chart} alt='line' /></span>
+    </div>
+    <div className='listItems'>
+        <p style={{fontSize:'14px',fontWeight:'700',marginRight:'7%',marginBottom:'3%',marginLeft:'1.5%'}}>$40.000</p>
+        <span><img src={Chart} alt='line' /></span>
+    </div>
+    <div className='listItems'>
+        <p style={{fontSize:'14px',fontWeight:'700',marginRight:'7%',marginBottom:'3%',marginLeft:'1.5%'}}>$35.000</p>
+        <span><img src={Chart} alt='line' /></span>
+    </div>
+    <div className='listItems'>
+        <p style={{fontSize:'14px',fontWeight:'700',marginRight:'7%',marginBottom:'3%',marginLeft:'1.5%'}}>$20.000</p>
+        <span><img src={Chart} alt='line' /></span>
+    </div>
+    <div className='listItems'>
+        <p style={{fontSize:'14px',fontWeight:'700',marginRight:'7%',marginBottom:'3%',marginLeft:'1.5%'}}>$10.000</p>
+        <span><img src={Chart} alt='line' /></span>
+    </div>
+    <div className='listItems'>
+        <p style={{fontSize:'14px',fontWeight:'700',marginRight:'7%',marginBottom:'3%',marginLeft:'1.5%'}}>$5.000</p>
+        <span><img src={Chart} alt='line' /></span>
+    </div>
+    <div className='listItems'>
+        <p style={{fontSize:'14px',fontWeight:'700',marginRight:'7%',marginBottom:'3%',marginLeft:'1.5%'}}>$0.000</p>
+        <span><img src={Chart} alt='line' /></span>
+    </div>
+    </div>
+    <img/>
+    <div className='days'>
+        <p>Sun</p>
+        <p>Mon</p>
+        <p>Tue</p>
+        <p>Wed</p>
+        <p style={{fontSize:'14px',fontWeight:'700'}} className='highlight'>Thu</p>
+        <p>Fri</p>
+        <p>Sat</p>
+    </div>
+    
+    </div>
+    </>
   )
 }
 
